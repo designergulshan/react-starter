866e0a5c8ce90fdd78229acd0e3e4231e247ea3d
Gulshan kumar <kumar.gulshan80@gmail.com>
2018-09-11 09:55:37 +0530

    Initial React Setup.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./scss/main.scss";
+
+const Index = () => {
+  return <h1>Hello React</h1>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
